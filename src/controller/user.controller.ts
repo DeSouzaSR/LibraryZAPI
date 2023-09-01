3aa83b48309425a95c56ed1715e41e50c9804a5b
Sandro Ricardo De Souza <sandro.fisica@gmail.com>
2023-09-01 16:36:04 -0300

    Create routers

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,18 +1,27 @@
-import { NextFunction, Request, Response } from "express";
+import e, { NextFunction, Request, Response } from "express";
 import User from "../database/models/User";
 import UserService from "../services/user.service";
 
 class UserController {
-    private service = new UserService()
+  private service = new UserService();
 
-    async get(req: Request, res: Response, next: NextFunction) {
-        try {
-            const { status, message } = await this.service.get()
-            res.status(status).json(message) 
-        } catch (error) {
-            next(error)
-        }
-    } 
+  async get(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { status, message } = await this.service.get();
+      res.status(status).json(message);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async login(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { status, message } = await this.service.login(req.body);
+      res.status(status).json(message);
+    } catch (error) {
+      next(e);
+    }
+  }
 }
 
-export default UserController
+export default UserController;
